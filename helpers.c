0a85464a091399ee33ff75fac4032c0d01de2dad
Rob Rose <RobRoseKnows@gmail.com>
2018-11-29 16:36:37 -0500

    Worked on helpers some.

--- helpers.c
+++ helpers.c
@@ -1,8 +1,17 @@
-int mul_wo_mul(int a, int b) {
-    int result = 0;
-    for (int i = b; i > 0; i--) {
-        result += a;
+// This is a multiply function that multiplies a float by an integer by repeatedly
+// adding it. Only the float can be negative or have decimal.
+float mul_wo_mul(float can_neg, int non_neg) {
+    float result = 0;
+    for (int i = non_neg; i > 0; i--) {
+        // Add repeatedly
+        result += can_neg;
     }
-    // Return result in
+    // We'll return the result in a register for reals, bbut lets treat it like a
+    // function for simplicity for now.
     return result;
+}
+
+// This is just an example main() method.
+int main() {
+    const float x = mul_wo_mul(1.5, 3);
 }
